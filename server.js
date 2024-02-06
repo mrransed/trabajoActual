6fdc65e8e5158c7c3197792660553732a1db2f47
ransed <ransedweb@gmail.com>
2024-02-06 17:35:11 -0500

    archivo base del proyecto

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json());
+
+// Middleware para manejar CORS (Cross-Origin Resource Sharing)
+
+
+
+app.get('/', function(req, res){
+    res.send("funciona el servidor")
+})
+
+const PORT= process.env.PORR || 3002
+app.listen(port,()=>{
+    console.log('listening on port '+PORT);
+})
